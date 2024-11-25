50ff9855bb2bbd019606a8110f7503e20d842e85
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2024-11-25 09:15:25 -0700

    added html form and some function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
+import { React, useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName);
+  };
+
   return (
     <div className="App">
       <h1>React Submission Form</h1>
       <fieldset>
         <form action="#" method="get">
           <label for="firstname">First Name*</label>
-          <input type="text" />
-          <button type="submit" value="Submit">
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="Enter First Name"
+            required
+          />
+          <button type="submit" value="Submit" onClick={(e) => handleSubmit(e)}>
             Submit
           </button>
         </form>
